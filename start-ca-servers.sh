77c24899f4c55d2c44054c780dc4c4b9ff64824e
mbizhani <mbizhani@gmail.com>
2021-02-13 16:31:57 +0330

    refactor registerEnroll.sh

--- start-ca-servers.sh
+++ start-ca-servers.sh
@@ -21,25 +21,25 @@ waitForChart "ca-orderer"
 waitForChart "ca-org1"
 waitForChart "ca-org2"
 
-sleep 2
+sleep 6
 
 CA_ORDERER_POD="$(kubectl get pod -l app.kubernetes.io/instance=ca-orderer -o jsonpath="{.items[0].metadata.name}")"
 CA_ORG1_POD="$(kubectl get pod -l app.kubernetes.io/instance=ca-org1 -o jsonpath="{.items[0].metadata.name}")"
 CA_ORG2_POD="$(kubectl get pod -l app.kubernetes.io/instance=ca-org2 -o jsonpath="{.items[0].metadata.name}")"
 
-kubectl exec "${CA_ORDERER_POD}" -- sh -c "
+kubectl exec "${CA_ORDERER_POD}" -- bash -c "
   . /hlf/fabric-ca/registerEnroll.sh
   createOrderer
   echo \"*** Orderer MSP Finished\"
 "
 
-kubectl exec "${CA_ORG1_POD}" -- sh -c "
+kubectl exec "${CA_ORG1_POD}" -- bash -c "
   . /hlf/fabric-ca/registerEnroll.sh
   createOrg1
   echo \"*** Org1 MSP Finished\"
 "
 
-kubectl exec "${CA_ORG2_POD}" -- sh -c "
+kubectl exec "${CA_ORG2_POD}" -- bash -c "
   . /hlf/fabric-ca/registerEnroll.sh
   createOrg2
   echo \"*** Org2 MSP Finished\"
