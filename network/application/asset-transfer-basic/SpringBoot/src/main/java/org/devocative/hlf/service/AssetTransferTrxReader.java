815c80cf30fdd9d58d3f1f86c3cd672354d85d28
mbizhani <mbizhani@gmail.com>
2022-01-17 20:38:20 +0330

    apply refactored transaction reader API

--- network/application/asset-transfer-basic/SpringBoot/src/main/java/org/devocative/hlf/service/AssetTransferTrxReader.java
+++ network/application/asset-transfer-basic/SpringBoot/src/main/java/org/devocative/hlf/service/AssetTransferTrxReader.java
@@ -1,15 +1,19 @@
 package org.devocative.hlf.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.devocative.thallo.hlf.dto.HlfTransactionInfo;
 import org.devocative.thallo.hlf.iservice.IHlfTransactionReader;
+import org.devocative.thallo.hlf.service.HlfTransactionReaderHandler;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class AssetTransferTrxReader implements IHlfTransactionReader {
+
 	@Override
-	public void handleTransaction(HlfTransactionInfo info) {
-		log.info("## Asset Info in Ledger: {}", info);
+	public void handleTransaction(HlfTransactionReaderHandler handler) {
+		handler.readBlockFrom(0, info -> log.info("OldBlock: {}", info));
+
+		handler.registerBlockListener(info -> log.info("NewBlock: {}", info));
 	}
+
 }
