4d0780b70caf64b996799b1ac8edfbd17add3247
mbizhani <mbizhani@gmail.com>
2023-05-31 20:23:10 +0330

    support only spring boot chaincode

--- redeploy-ext-cc.sh
+++ redeploy-ext-cc.sh
@@ -3,35 +3,15 @@
 source .env
 source common.sh
 
-if [ "${CC_LANG}" == "go" ]; then
-  pushd network/chaincode/asset-transfer-basic/go
-  docker rmi "${CC_DOCKER_IMAGE}:${CC_DOCKER_TAG}" || true
-  docker rmi "${CC_DOCKER_PUSH_IMAGE}:${CC_DOCKER_TAG}" || true
-  docker build \
-    --build-arg GOPROXY="${GOPROXY}" \
-    -t "${CC_DOCKER_PUSH_IMAGE}:${CC_DOCKER_TAG}" .
+pushd network/chaincode/asset-transfer-basic/SpringBoot
+docker rmi "${CC_DOCKER_IMAGE}:${CC_DOCKER_TAG}" || true
+docker rmi "${CC_DOCKER_PUSH_IMAGE}:${CC_DOCKER_TAG}" || true
+mvn clean package
+docker build -t "${CC_DOCKER_PUSH_IMAGE}:${CC_DOCKER_TAG}" .
 
-  if [ "${REG_USER}" ]; then
-    docker login -u "${REG_USER}" -p "${REG_PASS}" "${REG_URL}"
-  fi
-  docker push "${CC_DOCKER_PUSH_IMAGE}:${CC_DOCKER_TAG}"
-  popd
-elif [ "${CC_LANG}" == "java" ]; then
-  pushd network/chaincode/asset-transfer-basic/SpringBoot
-  docker rmi "${CC_DOCKER_IMAGE}:${CC_DOCKER_TAG}" || true
-  docker rmi "${CC_DOCKER_PUSH_IMAGE}:${CC_DOCKER_TAG}" || true
-  mvn clean package
-  docker build -t "${CC_DOCKER_PUSH_IMAGE}:${CC_DOCKER_TAG}" .
-
-  if [ "${REG_USER}" ]; then
-    docker login -u "${REG_USER}" -p "${REG_PASS}" "${REG_URL}"
-  fi
-  docker push "${CC_DOCKER_PUSH_IMAGE}:${CC_DOCKER_TAG}"
-  popd
-else
-  echo "ERROR: Invalid Chaincode Lang: ${CC_LANG}"
-  exit 1
-fi
+docker login -u "${REG_USER}" -p "${REG_PASS}" "${REG_PUSH_URL}"
+docker push "${CC_DOCKER_PUSH_IMAGE}:${CC_DOCKER_TAG}"
+popd
 
 helm upgrade basic helms/hlf-cc \
   --atomic \
